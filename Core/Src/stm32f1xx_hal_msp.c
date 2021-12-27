85125f60dab3cb3839d8460d37b99959cd7e3c34
Kanishk Kaushal <75947299+Kanishk-Kaushal@users.noreply.github.com>
2021-12-27 23:51:43 +0530

    Add files via upload

--- Core/Src/stm32f1xx_hal_msp.c
+++ Core/Src/stm32f1xx_hal_msp.c
@@ -72,9 +72,9 @@ void HAL_MspInit(void)
 
   /* System interrupt init*/
 
-  /** DISABLE: JTAG-DP Disabled and SW-DP Disabled
+  /** NOJTAG: JTAG-DP Disabled and SW-DP Enabled
   */
-  __HAL_AFIO_REMAP_SWJ_DISABLE();
+  __HAL_AFIO_REMAP_SWJ_NOJTAG();
 
   /* USER CODE BEGIN MspInit 1 */
 
@@ -87,33 +87,55 @@ void HAL_MspInit(void)
 * @param hi2c: I2C handle pointer
 * @retval None
 */
-//void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
-//{
-//  GPIO_InitTypeDef GPIO_InitStruct = {0};
-//  if(hi2c->Instance==I2C1)
-//  {
-//  /* USER CODE BEGIN I2C1_MspInit 0 */
-////
-//  /* USER CODE END I2C1_MspInit 0 */
-//
-//    __HAL_RCC_GPIOB_CLK_ENABLE();
-//    /**I2C1 GPIO Configuration
-//    PB6     ------> I2C1_SCL
-//    PB7     ------> I2C1_SDA
-//    */
-//    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
-//    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-//    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-//
-//    /* Peripheral clock enable */
-//    __HAL_RCC_I2C1_CLK_ENABLE();
-//  /* USER CODE BEGIN I2C1_MspInit 1 */
-////
-//  /* USER CODE END I2C1_MspInit 1 */
-//  }
-//
-//}
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspInit 0 */
+
+  /* USER CODE END I2C1_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+  /* USER CODE BEGIN I2C1_MspInit 1 */
+
+  /* USER CODE END I2C1_MspInit 1 */
+  }
+  else if(hi2c->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspInit 0 */
+
+  /* USER CODE END I2C2_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C2 GPIO Configuration
+    PB10     ------> I2C2_SCL
+    PB11     ------> I2C2_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C2_CLK_ENABLE();
+  /* USER CODE BEGIN I2C2_MspInit 1 */
+
+  /* USER CODE END I2C2_MspInit 1 */
+  }
+
+}
 
 /**
 * @brief I2C MSP De-Initialization
@@ -121,30 +143,50 @@ void HAL_MspInit(void)
 * @param hi2c: I2C handle pointer
 * @retval None
 */
-//void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
-//{
-//  if(hi2c->Instance==I2C1)
-//  {
-//  /* USER CODE BEGIN I2C1_MspDeInit 0 */
-////
-//  /* USER CODE END I2C1_MspDeInit 0 */
-//    /* Peripheral clock disable */
-//    __HAL_RCC_I2C1_CLK_DISABLE();
-//
-//    /**I2C1 GPIO Configuration
-//    PB6     ------> I2C1_SCL
-//    PB7     ------> I2C1_SDA
-//    */
-//    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6);
-//
-//    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_7);
-//
-//  /* USER CODE BEGIN I2C1_MspDeInit 1 */
-////
-//  /* USER CODE END I2C1_MspDeInit 1 */
-//  }
-//
-//}
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+{
+  if(hi2c->Instance==I2C1)
+  {
+  /* USER CODE BEGIN I2C1_MspDeInit 0 */
+
+  /* USER CODE END I2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_7);
+
+  /* USER CODE BEGIN I2C1_MspDeInit 1 */
+
+  /* USER CODE END I2C1_MspDeInit 1 */
+  }
+  else if(hi2c->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspDeInit 0 */
+
+  /* USER CODE END I2C2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C2_CLK_DISABLE();
+
+    /**I2C2 GPIO Configuration
+    PB10     ------> I2C2_SCL
+    PB11     ------> I2C2_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_10);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_11);
+
+  /* USER CODE BEGIN I2C2_MspDeInit 1 */
+
+  /* USER CODE END I2C2_MspDeInit 1 */
+  }
+
+}
 
 /**
 * @brief UART MSP Initialization
